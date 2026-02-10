39f7e181a3b6b24d8fdb51db20e3a4a6cf6323b4
Max K <mxkk@pm.me>
2026-02-10 09:11:58 -0500

    Update 10_leapYears js to pass tests

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // divisible by 4 and 400, but not 100
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
