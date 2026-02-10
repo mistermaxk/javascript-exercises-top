a2c164c0b6ae1f5df88a8ebeb20efe7c1c7ffad9
Max K <mxkk@pm.me>
2026-02-09 20:51:09 -0500

    Update 09_summAll js to solve

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || !Number.isInteger(a) ||
+        b < 0 || !Number.isInteger(b)) {
+             return 'ERROR';
+        } else {
+            if (Math.min(a, b) === b) [a, b] = [b, a];
+            let accumulator = 0;
+            for (let i = a; i <= b; i++) {
+                accumulator += i;
+            }
+            return accumulator;
+    }
 };
 
 // Do not edit below this line
