2fc984860d2d504f51649beecdfb0ad5bcafd020
Max K <mxkk@pm.me>
2026-02-10 09:27:58 -0500

    Update 11_tempComversion js to pass tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhenheit) {
+  // C = 5/9(F-32)
+  let celsius = (5/9 * (farhenheit - 32));
+  return Math.round(celsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // F = (c * 9/5) + 32
+  let farhenheit = ((celsius * 9/5) + 32);
+  return Math.round(farhenheit * 10) / 10;
 };
 
 // Do not edit below this line
