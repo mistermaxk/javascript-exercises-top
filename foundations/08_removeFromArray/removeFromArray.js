af2bba767b64110a30820f46cd4e128c1ef86ee0
Max K <mxkk@pm.me>
2026-02-09 20:25:20 -0500

    Update 08_removeFromArray js to solve

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, toRemove) {
+    for (const arg of arguments) {
+        if (arr.includes(arg)) {
+            arr = arr.filter(num => num !== arg);
+        }
+    }
+    return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
