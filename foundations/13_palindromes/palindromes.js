0fadd701ea6a69681970c6977cd842ae68f8bf8b
Max K <mxkk@pm.me>
2026-02-14 09:56:58 -0500

    Update 13_palindromes js to pass tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let wordRevised = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return wordRevised === wordRevised.split("").reverse().join("");
 };
 
 // Do not edit below this line
