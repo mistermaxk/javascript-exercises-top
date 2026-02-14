83671d910e9cde8bf0ce8d06fc981a998a713490
Max K <mxkk@pm.me>
2026-02-14 09:14:01 -0500

    Update 12_calculator js to pass tests

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let total = base;
+  for (let i = 1; i < exp; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	return Array.from( {length: num}, (v, i) => i + 1)
+              .reduceRight((accum, item) => accum *= item, 1);
 };
 
 // Do not edit below this line
