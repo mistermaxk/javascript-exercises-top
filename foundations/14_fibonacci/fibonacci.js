6ec8690da0097eec803e4afbb405b4574cb603b7
Max K <mxkk@pm.me>
2026-02-15 13:50:14 -0500

    Update 14_fibonacci js to pass all tests

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(seriesNumber) {
+// beginning with 0, sum of two previous numbers
+    if (Number(seriesNumber) < 0) return "OOPS";
+    let fibArray = [];
+    for (let i = 0; i <= Number(seriesNumber); i++) {
+        if (i === 0 || i === 1) fibArray.push(i);
+        else fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+    return fibArray[seriesNumber];
 };
 
 // Do not edit below this line
