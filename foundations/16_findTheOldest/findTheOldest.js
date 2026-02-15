1131fc20b112ca1686d3ba436ab8eb34c5031cd7
Max K <mxkk@pm.me>
2026-02-15 14:35:13 -0500

    Update 16_findTheOldest js to pass all tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (let person of people) {
+        if (person.yearOfDeath === undefined) person.yearOfDeath = new Date().getFullYear();
+    }
+    people.sort((a, b) => (b.yearOfDeath-b.yearOfBirth) - (a.yearOfDeath-a.yearOfBirth));
+    return people[0];
 };
 
 // Do not edit below this line
